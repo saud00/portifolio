080fb741306e02c3720ab62101e189295d0e4cc7
saud <gomail2017@gmail.com>
2023-09-23 07:33:11 -0700

    updated resumee

--- components/Resume.tsx
+++ components/Resume.tsx
@@ -5,9 +5,9 @@ type Props = {}
 const Resume = (props: Props) => {
   return (
     <div className='dark:dark-text-white my-5 px-5'>
-        <Link href='/saud_cv.pdf'>
-         <a target="_blank" className='bg-gradient-to-r from-stone-800 to-gray-500 hover:from-gray-500  hover:to-stone-800' download> Click on me to <strong> <i  >download resume</i> </strong> </a>
-        </Link>
+        {/* <Link href='/saud_cv.pdf' > */}
+         <a download target="_blank" href='/saud_cv.pdf' className='bg-gradient-to-r from-stone-800 to-gray-500 hover:from-gray-500  hover:to-stone-800' > Click on me to <strong> <i  >download resume</i> </strong> </a>
+        {/* </Link> */}
     </div>
   )
 }
